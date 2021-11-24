cd79b278871480c7da566132662c66c6dee4c1e9
Daniel Rojo <danielrojo@gmail.com>
2021-11-24 10:20:30 +0100

    Calculator service added

--- src/app/components/calculator/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator/calculator.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CalculatorService } from 'src/app/services/calculator.service';
 
-enum State {
-  INIT,
-  FIRST_FIGURE,
-  SECOND_FIGURE,
-  RESULT
-}
+
 
 @Component({
   selector: 'app-calculator',
@@ -16,115 +12,13 @@ export class CalculatorComponent implements OnInit {
 
   display = '';
 
-  currentState = State.INIT;
-  firstFigure = 0;
-  secondFigure = 0;
-  operator = '';
-  result: number = 0;
-
-  constructor() { }
+  constructor(private service: CalculatorService) { }
 
   ngOnInit(): void {
   }
 
-  handleNumber(value: number) {
-
-    switch (this.currentState) {
-      case State.INIT:
-        this.firstFigure = value;
-        this.display += value.toString();
-        this.currentState = State.FIRST_FIGURE;
-        break;
-      case State.FIRST_FIGURE:
-        this.firstFigure = this.firstFigure * 10 + value;
-        this.display += value.toString();
-        break;
-      case State.SECOND_FIGURE:
-        this.secondFigure = this.secondFigure * 10 + value;
-        this.display += value.toString();
-        break;
-      case State.RESULT:
-        this.firstFigure = value;
-        this.secondFigure = 0;
-        this.operator = '';
-        this.result = 0;
-        this.display = value.toString();
-        this.currentState = State.FIRST_FIGURE;
-        break;
-    }
-
-  }
-
-  getResult(): number {
-    switch (this.operator) {
-      case '+':
-        return this.firstFigure + this.secondFigure;
-      case '-':
-        return this.firstFigure - this.secondFigure;
-      case '*':
-        return this.firstFigure * this.secondFigure;
-      case '/':
-        return this.firstFigure / this.secondFigure;
-      default:
-        break;
-    }
-    return 0;
-  }
-
-  isOperator(value: string): boolean {
-    return value === '+' || value === '-' || value === '*' || value === '/';
-  }
-
-  clear() {
-    this.firstFigure = 0;
-    this.secondFigure = 0;
-    this.operator = '';
-    this.result = 0;
-    this.display = '';
-    this.currentState = State.INIT;
-  }
-
-  handleSymbol(value: string) {
-    if (value === 'c') {
-      this.clear();
-    }
-    switch (this.currentState) {
-      case State.INIT:
-        break;
-      case State.FIRST_FIGURE:
-        if (this.isOperator(value)) {
-          this.operator = value;
-          this.display += value;
-          this.currentState = State.SECOND_FIGURE;
-        }
-        break;
-      case State.SECOND_FIGURE:
-        if (value === '=') {
-          this.result = this.getResult();
-          this.display += value + this.result.toString();
-          this.currentState = State.RESULT;
-        }
-        break;
-      case State.RESULT:
-        if (this.isOperator(value)) {
-          this.firstFigure = this.result;
-          this.operator = value;
-          this.secondFigure = 0;
-          this.result = 0;
-          this.display = this.firstFigure.toString() + value;
-          this.currentState = State.SECOND_FIGURE;
-        }
-        break;
-    }
-
-  }
-
   onClick(myValue: string | number) {
-    if (typeof myValue === 'string') {
-      this.handleSymbol(myValue);
-    } else if (typeof myValue === 'number') {
-      this.handleNumber(myValue);
-    }
+    this.display = this.service.process(myValue);
   }
 
 
---
+++ src/app/services/calculator.service.ts
@@ -0,0 +1,126 @@
+import { Injectable } from '@angular/core';
+
+enum State {
+  INIT,
+  FIRST_FIGURE,
+  SECOND_FIGURE,
+  RESULT
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculatorService {
+
+  private currentState = State.INIT;
+  private firstFigure = 0;
+  private secondFigure = 0;
+  private operator = '';
+  private result: number = 0;
+
+  private display = '';
+
+  constructor() { }
+
+  private handleNumber(value: number) {
+
+    switch (this.currentState) {
+      case State.INIT:
+        this.firstFigure = value;
+        this.display += value.toString();
+        this.currentState = State.FIRST_FIGURE;
+        break;
+      case State.FIRST_FIGURE:
+        this.firstFigure = this.firstFigure * 10 + value;
+        this.display += value.toString();
+        break;
+      case State.SECOND_FIGURE:
+        this.secondFigure = this.secondFigure * 10 + value;
+        this.display += value.toString();
+        break;
+      case State.RESULT:
+        this.firstFigure = value;
+        this.secondFigure = 0;
+        this.operator = '';
+        this.result = 0;
+        this.display = value.toString();
+        this.currentState = State.FIRST_FIGURE;
+        break;
+    }
+
+  }
+
+  private getResult(): number {
+    switch (this.operator) {
+      case '+':
+        return this.firstFigure + this.secondFigure;
+      case '-':
+        return this.firstFigure - this.secondFigure;
+      case '*':
+        return this.firstFigure * this.secondFigure;
+      case '/':
+        return this.firstFigure / this.secondFigure;
+      default:
+        break;
+    }
+    return 0;
+  }
+
+  private isOperator(value: string): boolean {
+    return value === '+' || value === '-' || value === '*' || value === '/';
+  }
+
+  private clear() {
+    this.firstFigure = 0;
+    this.secondFigure = 0;
+    this.operator = '';
+    this.result = 0;
+    this.display = '';
+    this.currentState = State.INIT;
+  }
+
+  private handleSymbol(value: string) {
+    if (value === 'c') {
+      this.clear();
+    }
+    switch (this.currentState) {
+      case State.INIT:
+        break;
+      case State.FIRST_FIGURE:
+        if (this.isOperator(value)) {
+          this.operator = value;
+          this.display += value;
+          this.currentState = State.SECOND_FIGURE;
+        }
+        break;
+      case State.SECOND_FIGURE:
+        if (value === '=') {
+          this.result = this.getResult();
+          this.display += value + this.result.toString();
+          this.currentState = State.RESULT;
+        }
+        break;
+      case State.RESULT:
+        if (this.isOperator(value)) {
+          this.firstFigure = this.result;
+          this.operator = value;
+          this.secondFigure = 0;
+          this.result = 0;
+          this.display = this.firstFigure.toString() + value;
+          this.currentState = State.SECOND_FIGURE;
+        }
+        break;
+    }
+
+  }
+
+  public process(myValue: string | number): string {
+    if (typeof myValue === 'string') {
+      this.handleSymbol(myValue);
+    } else if (typeof myValue === 'number') {
+      this.handleNumber(myValue);
+    }
+    return this.display;
+  }
+
+}

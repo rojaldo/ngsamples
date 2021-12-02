91a276e6aca5638b56e57a742f338c669286708e
Daniel Rojo <danielrojo@gmail.com>
2021-12-02 16:26:37 +0100

    cards provide fedback to answers

--- src/app/components/trivial/trivial-card/trivial-card.component.ts
+++ src/app/components/trivial/trivial-card/trivial-card.component.ts
@@ -9,9 +9,39 @@ import { Card } from 'src/model/card';
 export class TrivialCardComponent implements OnInit {
 
   @Input() card: Card = new Card();
+  classes: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.classes = [];
+    for (const answer of this.card.answers) {
+      this.classes.push('btn btn-primary btn-lg btn-block');
+    }
+  }
+
+  onAnswer(answer: string) {
+    this.card.responded = true;
+    this.card.rightAnswered = (answer === this.card.correctAnswer);
+    this.card.userAnswer = answer;
+    this.classes = [];
+    for (const answer of this.card.answers) {
+      this.classes.push(this.getClass(answer))
+    }
+  }
+
+  getClass(answer: string): string {
+    if (this.card.responded) {
+      if (this.card.correctAnswer === answer) {
+        return 'btn btn-success btn-lg btn-block';
+      } else if (this.card.userAnswer === answer) {
+        return 'btn btn-danger btn-lg btn-block';
+      }
+      return 'btn btn-secondary btn-lg btn-block';
+    } else {
+      return 'btn btn-primary btn-lg btn-block';
+    }
+
   }
 
 }
--- src/model/card.ts
+++ src/model/card.ts
@@ -1,10 +1,16 @@
 export class Card {
     question = '';
+    correctAnswer = '';
     answers: string[] = [];
+    responded = false;
+    rightAnswered!: boolean; 
+    userAnswer: string = '';
+
     constructor (json?: any) {
         if (json) {
             this.question = json.question;
             this.answers = json.incorrect_answers;
+            this.correctAnswer = json.correct_answer;
             this.answers.push(json.correct_answer);    
         }
     }
